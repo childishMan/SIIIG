7c8422380b252325c141d8410816ed9828743b20
ChildishMan <gleam2k19@gmail.com>
2020-04-30 17:02:53 +0300

    added project with custom controls

---
+++ siig/CControl/CControl/MContainer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomControls
+{
+    public class MContainer : ContentControl
+    {
+        static MContainer()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MContainer), new FrameworkPropertyMetadata(typeof(MContainer)));
+        }
+
+
+        public int Radius
+        {
+            get { return (int)GetValue(RadiusProperty); }
+            set { SetValue(RadiusProperty, value); }
+        }
+
+        public static readonly DependencyProperty RadiusProperty =
+            DependencyProperty.Register("Radius", typeof(int), typeof(MContainer), new PropertyMetadata(0,null,new CoerceValueCallback(CoerceRadius)));
+
+        private static object CoerceRadius(DependencyObject d, object basevalue)
+        {
+            if ((int)basevalue < 0)
+            {
+                return 0;
+            }
+
+            return basevalue;
+        }
+
+
+    }
+}
---
+++ siig/CControl/CControl/OutputBox.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CustomControls
+{
+    public class OutputBox : Control
+    {
+        static OutputBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(OutputBox), new FrameworkPropertyMetadata(typeof(OutputBox)));
+        }
+
+
+
+        public string content
+        {
+            get { return (string)GetValue(contentProperty); }
+            set { SetValue(contentProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for content.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty contentProperty =
+            DependencyProperty.Register("content", typeof(string), typeof(OutputBox), new PropertyMetadata(""));
+
+
+    }
+}
